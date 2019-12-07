15ae6557aa880e8ede8aae28995e4200c0e53d70
clairemuller <clairermuller@gmail.com>
2019-12-06 16:16:51 -0800

    add concurrency

--- main.go
+++ main.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"sort"
 	"strings"
 	"time"
 
@@ -55,18 +56,39 @@ func getTopStories(numStories int) ([]item, error) {
 	if err != nil {
 		return nil, errors.New("Failed to load top stories")
 	}
+	type result struct {
+		index int
+		item  item
+		err   error
+	}
+	resultCh := make(chan result)
+
+	for i := 0; i < numStories; i++ {
+		go func(index, id int) {
+			hnItem, err := client.GetItem(id)
+			if err != nil {
+				resultCh <- result{index: index, err: err}
+			}
+			resultCh <- result{index: index, item: parseHNItem(hnItem)}
+		}(i, ids[i])
+	}
+
+	var results []result
+	for i := 0; i < numStories; i++ {
+		results = append(results, <-resultCh)
+	}
+	sort.Slice(results, func(i, j int) bool {
+		return results[i].index < results[j].index
+	})
+
 	var stories []item
-	for _, id := range ids {
-		hnItem, err := client.GetItem(id)
-		if err != nil {
+
+	for _, res := range results {
+		if res.err != nil {
 			continue
 		}
-		item := parseHNItem(hnItem)
-		if isStoryLink(item) {
-			stories = append(stories, item)
-			if len(stories) >= numStories {
-				break
-			}
+		if isStoryLink(res.item) {
+			stories = append(stories, res.item)
 		}
 	}
 	return stories, nil
